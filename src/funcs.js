ac2837d6a9f2f5a7839aac57ff694984a95198b5
fer <->
2021-04-14 23:13:32 -0300

    Add a Dropdown

--- src/funcs.js
+++ src/funcs.js
@@ -5,5 +5,15 @@ function addNewRow(rowData) {
   const ws = ss.getSheetByName("Results");
 
   ws.appendRow([rowData.item, rowData.qty, currentDate]);
+
+  return true;
   
+}
+
+function getDropDownArray(){
+
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const ws = ss.getSheetByName("Dropdown Options");
+  return ws.getRange(2, 2, ws.getLastRow()-1, 1).getValues();
+
 }
